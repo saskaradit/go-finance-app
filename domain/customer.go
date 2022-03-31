e8fc578703b5a69129e415b695ccb96f7a55453b
Raditya Saskara <saskaradit@gmail.com>
2022-03-31 15:09:50 +0700

    implement data transfer object

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/saskaradit/finance-app/errs"
+import (
+	"github.com/saskaradit/finance-app/dto"
+	"github.com/saskaradit/finance-app/errs"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -15,4 +18,27 @@ type CustomerRepository interface {
 	//  status == 1, status == 0, status == ""
 	FindAll(status string) ([]Customer, *errs.AppError)
 	ById(string) (*Customer, *errs.AppError)
+}
+
+func (c Customer) StatusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      statusAsText,
+	}
 }
